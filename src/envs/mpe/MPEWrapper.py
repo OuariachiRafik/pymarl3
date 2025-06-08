a82a606b928769bbda7c6565e22de8c43fda39be
Hammadi Rafik Ouariachi <115115206+OuariachiRafik@users.noreply.github.com>
2025-06-08 17:29:38 +0100

    Update MPEWrapper.py

--- src/envs/mpe/MPEWrapper.py
+++ src/envs/mpe/MPEWrapper.py
@@ -44,6 +44,9 @@ class MPEWrapper(MultiAgentEnv):
         return [self.current_obs[agent] for agent in self.env.agents]
 
     def get_state(self):
+         obs = self.get_obs()
+        if not obs:
+            raise ValueError("get_state() called before environment was reset or stepped.")
         return np.concatenate(self.get_obs())
 
     def get_avail_actions(self):
