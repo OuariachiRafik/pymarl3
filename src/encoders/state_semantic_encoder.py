40b2e190d722b466117be53bcf9ebed5ff4b455f
Hammadi Rafik Ouariachi <115115206+OuariachiRafik@users.noreply.github.com>
2025-08-21 18:26:22 +0100

    Update state_semantic_encoder.py

--- src/encoders/state_semantic_encoder.py
+++ src/encoders/state_semantic_encoder.py
@@ -31,8 +31,10 @@ class StateSemanticEncoder(nn.Module):
 
     def forward(self, ally_feats, enemy_feats,
                 ally_last_act_oh=None):
-        ally_feats = th.tensor(ally_feats, dtype=th.float32)
-        enemy_feats = th.tensor(enemy_feats, dtype=th.float32)
+        print("ally_feats_dimensions=", ally_feats)
+        print("enemy_feats_dimensions=", enemy_feats)
+        ally_feats = th.tensor(ally_feats, dtype=th.float32).unsqueeze(1) 
+        enemy_feats = th.tensor(enemy_feats, dtype=th.float32).unsqueeze(1) 
         if self.action_dim and ally_last_act_oh is not None:
             ally_feats = th.cat([ally_feats, ally_last_act_oh], dim=-1)
         a = self.ally_enc(ally_feats)      # [B,T,out/2]
