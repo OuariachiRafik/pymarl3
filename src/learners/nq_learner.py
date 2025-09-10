ffd67dcd2a76496036ade4eb8145b4c3d6c8cac1
Hammadi Rafik Ouariachi <115115206+OuariachiRafik@users.noreply.github.com>
2025-09-10 16:35:51 +0100

    Update nq_learner.py

--- src/learners/nq_learner.py
+++ src/learners/nq_learner.py
@@ -216,7 +216,7 @@ class NQLearner:
             )
 
         # ---- UPDATE CMI MASKER -----------------------------------------
-        if self.use_cmi_mask and causal_update > 5000 and causal_update % 1000==0:
+        if self.use_cmi_mask and  causal_update % 1000==0:
             # Build joint action one-hot per step: [B*T, n_agents * n_actions]
             B, T, _ = z_t.shape
             n_ag, n_ac = self.args.n_agents, self.args.n_actions
@@ -246,7 +246,7 @@ class NQLearner:
             self.causal_mask = self.cmi_masker.get_state_mask().detach().view(1, 1, -1)
 
 
-        if self.use_state_blocks and self.use_cmi_mask and self.use_intrinsic_rewards and causal_update > 5000 and causal_update % 1000==0:
+        if self.use_state_blocks and self.use_cmi_mask and self.use_intrinsic_rewards: #♥and causal_update > 5000 and causal_update % 1000==0:
             with th.no_grad():
                 # (i) compute per-transition gap on the whole mini-batch
                 gap = self.cmi_masker.prediction_gap(Z_flat, A_flat, Zp_flat, sum_over_k=True)  # [B*T]
@@ -278,7 +278,7 @@ class NQLearner:
             rewards_for_td = rewards
         
         if self.use_state_blocks:
-            if self.use_cmi_mask and causal_update > 5000 and causal_update % 1000==0:
+            if self.use_cmi_mask and causal_update > 1000: # and causal_update % 1000==0:
                 M = self.causal_mask # [1,1,dz]
                 print("Causal Mask Shape = ", M.shape)
                 print("Causal Mask = ", M)
