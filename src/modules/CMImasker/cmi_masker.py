11dbfa4bd0c9cb764174456ccd483b07a76a9215
Hammadi Rafik Ouariachi <115115206+OuariachiRafik@users.noreply.github.com>
2025-09-25 16:38:53 +0100

    Update cmi_masker.py

--- src/modules/CMImasker/cmi_masker.py
+++ src/modules/CMImasker/cmi_masker.py
@@ -8,6 +8,7 @@ import torch.nn as nn
 import torch.optim as optim
 import matplotlib.pyplot as plt; plt.switch_backend("Agg")
 from matplotlib.colors import ListedColormap, BoundaryNorm
+import wandb
 
 from .masked_predictor import ChildMaskedPredictor
 
